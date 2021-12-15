07582c72a9dbe6a3fc5eec6b3ca1635a55f4dab6
Danil <48606033+danbka763@users.noreply.github.com>
2021-12-15 12:03:11 +0700

    push code

---
+++ hw1.cpp
@@ -0,0 +1,25 @@
+﻿// Код написан на C++
+
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <math.h>
+#include <string> 
+
+using namespace std;
+int main()
+{
+    system("chcp 1251"); system("cls");
+
+    printf("Программу выполнил Жигалин Данил Вадимович, гр. 421-1\n\n");
+
+    string pi = to_string(M_PI);
+
+    for (int i = 0; i < pi.length(); i++) {
+        if (pi[i] == '0' || pi[i] == '5') {
+            pi.erase(i, 1);
+        }
+    }
+
+    cout << pi;
+}
